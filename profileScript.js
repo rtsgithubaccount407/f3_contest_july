5ca3d2b73d7d722fc8d2a4abfe57934e39076b75
Rohit Sharma <123295893+rtsgithubaccount407@users.noreply.github.com>
2023-07-29 23:26:03 +0530

    Add files via upload
    contest3_f3

---
+++ profileScript.js
@@ -0,0 +1,24 @@
+var blurred = false;
+window.onblur = function() { blurred = true; };
+window.onfocus = function() { blurred && (location.reload()); };
+
+function viewProfile() {
+    if (localStorage.getItem("token") === null) {
+        window.location.href = "index.html";
+    }
+
+    document.getElementsByTagName("BODY")[0].style.background = "linear-gradient(25deg, #5bf7db 0 50%, #F8FAF5 50% 100%)";
+    var profile = document.getElementById("profile-details");
+
+    profile.innerHTML =
+        `<div>Full Name : ${localStorage.username}</div>
+    <div>Email : ${localStorage.email}</div>
+    <div>Token : ${localStorage.token}</div>
+    <div>Password : ${localStorage.password}</div>`
+}
+
+function removeToken() {
+    localStorage.removeItem("token");
+    alert("done");
+    window.location.href = "index.html";
+}
